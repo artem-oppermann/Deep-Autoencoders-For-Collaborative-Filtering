d17c148349b809e7aef85e2af3c03b5dc69f5306
Artem Oppermann <artem.oppermann@uni-oldenburg.de>
2019-01-19 14:01:44 +0100

    delete

--- model_helper.py
+++
@@ -1,7 +0,0 @@
-import tensorflow as tf
-
-def _get_bias_initializer():
-    return tf.zeros_initializer()
-
-def _get_weight_initializer():
-    return tf.random_normal_initializer(mean=0.0, stddev=0.05)
